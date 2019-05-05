674540322d14ac660b98c164cddd69625d558914
sahilsamani <40260218+sahilsamani@users.noreply.github.com>
2019-05-05 13:36:38 +1000

    updated functions.php page
    updated functions.php page

--- functions.php
+++ functions.php
@@ -41,14 +41,5 @@ function redirectSubsToFrontend() {
 
 }
 
-function my_acf_google_map_api( $api ){
-	
-	$api['key'] = 'AIzaSyDbCkqU2xRDQyZ5fczZ0KRdsMGvT7t5u7w';
-	
-	return $api;
-	
-}
-
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 
